432b31e52a06db912c4b5ac4c739b16a936d0e14
Hrithik Khadka <hritik.khadka@centria.fi>
2023-08-06 22:00:50 +0300

    add POST endpoint for todo creation

---
+++ app/api/todos/route.js
@@ -0,0 +1,10 @@
+import connectMongo from "@/libs/mongodb";
+import Todo from "@/models/todo";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const { name } = await req.json();
+  await connectMongo();
+  await Todo.create({ name });
+  return NextResponse.json({ message: "todo created!" }, { status: 201 });
+}
--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,8 +1,8 @@
 import mongoose, { connect } from "mongoose";
 
-const connectMongo = () => {
+const connectMongo = async () => {
   try {
-    mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("connected to mongodb...");
   } catch {
     console.log(error);
