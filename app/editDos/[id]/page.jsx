fc3646c1f386428f541d05e5527ab8833026fafc
Hrithik Khadka <hritik.khadka@centria.fi>
2023-08-17 00:00:38 +0300

    add functionality to edit todos

--- app/editDos/[id]/page.jsx
+++ app/editDos/[id]/page.jsx
@@ -1,4 +1,23 @@
 import EditDosForm from "@/components/EditDosForm";
-export default function EditDos() {
-  return <EditDosForm />;
+
+const getTodosById = async (id) => {
+  try {
+    const rsp = await fetch(`http://localhost:3000/api/todos/${id}`, {
+      cache: "no-store",
+    });
+    if (!rsp.ok) {
+      throw new Error("Failed!");
+    }
+    return rsp.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function EditDos({ params }) {
+  const { id } = params;
+  const { todo } = await getTodosById(id);
+  const { name } = todo;
+
+  return <EditDosForm id={id} name={name} />;
 }
--- components/EditDosForm.jsx
+++ components/EditDosForm.jsx
@@ -1,7 +1,35 @@
-export default function EditDosForm() {
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function EditDosForm({ id, name }) {
+  const [newName, setNewName] = useState(name);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const rsp = await fetch(`http://localhost:3000/api/todos/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ newName }),
+      });
+      if (!rsp.ok) {
+        throw new Error("Failed to update");
+      }
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <form className="flex flex-col gap-4">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
       <input
+        onChange={(e) => setNewName(e.target.value)}
+        value={newName}
         className="border border-slate-500 px-8 py-2"
         placeholder="name"
         type="text"
